f8388b83b7253e66561e98fa91753e2aca06104c
Quentin Dusser <qdusser@NX-DOMAIN.LAN>
2020-10-16 16:20:59 +0200

    commit

---
+++ scripts/jsAction_4f916b14-32c8-4eb1-94a7-c09ad60487df.js
@@ -0,0 +1,28 @@
+﻿// Déclaration d'une fonction pour mettre les nombres avec 2 digits
+function gerer2digits(nombre) {
+     if (nombre < 10)
+    {
+        nombre = "0"+nombre;
+    }
+  return nombre;
+}
+
+// Récupération de la catégorie de l'animal'
+var categorieAnimal = context.variableManager.getValue("nomCategorie");
+if (categorieAnimal == null) {
+        context.fail("Aucune catégorie trouvée");
+}
+
+// Récupération du code l'animal'
+var animalAchete = context.variableManager.getValue("itemId_rand");
+if (animalAchete == null) {
+        context.fail("Aucun animal trouvé");
+}
+
+//R2cupération de la date
+var timestampAjout = new Date();
+
+var formatheuresMinutes=gerer2digits(timestampAjout.getHours())+":"+gerer2digits(timestampAjout.getMinutes());
+var formatDate = timestampAjout.getDate()+"/"+gerer2digits(timestampAjout.getMonth()+1)+"/"+timestampAjout.getFullYear() ;
+
+logger.debug("L'animal "+animalAchete+" de la categorie "+categorieAnimal+" a été acheté le "+formatDate+" à "+formatheuresMinutes);
